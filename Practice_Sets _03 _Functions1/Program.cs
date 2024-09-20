ff1de2a72236f47b987e717108fae716508de4a1
Esumandi <sumandieugene@gmail.com>
2024-09-21 00:43:14 +0800

    “”

---
+++ Practice_Sets _03 _Functions1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Practice_Sets__03__Functions1;
+
+class Program
+{
+    public static double triangle(double a, double b, double c)
+    {
+        //s
+        double s = (a + b + c) / 2;
+        //formula
+        double Area = s *  (s - a ) * (s - b) * (s - c);
+        //sqr
+        double triangleArea = Math.Sqrt(Area);
+        //return
+        return  (double)triangleArea;
+    }
+
+    static void Main(string[] args)
+
+    {
+        while (true)
+        {
+
+
+
+            Program p = new Program();
+            // input
+            Console.WriteLine("Enter the length of the triangle to find the area. ");
+            
+            Console.Write("Enter lenght of A: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Enter lenght of B :");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Enter lenght of C: ");
+            double c = double.Parse(Console.ReadLine());
+            //output
+            double area = triangle(a, b, c);
+            Console.WriteLine($"The area is {area}");
+
+        }
+
+    }
+}
---
+++ Practice_Sets_03_Functions2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.Design;
+
+namespace Practice_Sets_02_Functions2;
+
+class Program
+
+{
+    static int leapyear(int year)
+    {
+        
+        if (year % 4 == 0) // && year % 100 != 0 && year % 400 == 0)
+        {
+
+            if (year % 100 != 0)
+            {
+                Console.WriteLine($"The year {year} is a leap year.");                
+            }
+            else
+            {
+                if (year % 400 == 0)
+                {
+                    Console.WriteLine($"The year {year} is a leap year.");
+                }
+                else
+                {
+                    Console.WriteLine($"The year {year} is not a leap year.");
+                }
+            }
+            
+        }
+        else
+        {
+            Console.WriteLine($"The year {year} is not a leap year.");
+        }
+        return year;
+    }
+    
+    
+    
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Program program = new Program();
+            Console.Write("Enter the year you want to check if its leap year or not: ");
+            int year = int.Parse(Console.ReadLine());
+            year = leapyear(year);
+        }
+
+    }
+}
